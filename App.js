aff23f304916708c7706e295cd84d7436a6ab386
Milan Ram <milanram0780@gmail.com>
2023-01-31 20:39:33 +0530

    website-2

--- App.js
+++ App.js
@@ -1,46 +1,6 @@
+const nav =document.getElementById('navbar');
+const navbtn =document.getElementById('home-nav-link');
 
-
-      const left_arrow = document.getElementById('image-scroll-arrow-left');
-      const right_arrow = document.getElementById('image-scroll-arrow-right');
-      const image_container =document.getElementById('scroll-images-container');
-      let num=0;
-      left_arrow.addEventListener('click',()=>{
-        if(num==0){
-        image_container.style.transform = "translateX(" + (-100) + "vw)";
-        num +=1;
-      }
-      else{
-          image_container.style.transform = "translateX(" + (0) + "px)";
-          num -=1;
-        }
-      })
-      right_arrow.addEventListener('click',()=>{
-       if(num==1){
-         image_container.style.transform = "translateX(" + (0) + "vw)";
-        num -=1;
-      }
-      else{
-        image_container.style.transform = "translateX(" + (-100) + "vw)";
-        num +=1;
-
-       }
-       function auto_slide(){
-        alert('hi');
-        image_container.style.transform = "translateX(" + (-100) + "vw)";
-       }
-       setTimeout(auto_slide, 2000)
-
-      // =============================== Mobile Menu ===================== //
-
-        })
-        const menu_icon =document.getElementById('mobile-menu-icon');
-        const close_icon =document.getElementById('mobile-close-icon');
-        const header =document.querySelector('.header');
-
-        menu_icon.addEventListener('click',()=>{
-          header.style.display ="block";
-        });
-        close_icon.addEventListener('click',()=>{
-          header.style.display ="none";
-        })
-   
+navbtn.addEventListener('click',()=>{
+  nav.classList.toggle('open-drop-down')
+})
