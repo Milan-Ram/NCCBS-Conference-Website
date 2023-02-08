609644e47fb9bd96ed9af0980744c515943861b6
Milan Ram <99471892+Milan-Ram@users.noreply.github.com>
2023-02-08 11:53:13 +0530

    Delete App.js

--- App.js
+++
@@ -1,6 +0,0 @@
-const nav =document.getElementById('navbar');
-const navbtn =document.getElementById('home-nav-link');
-
-navbtn.addEventListener('click',()=>{
-  nav.classList.toggle('open-drop-down')
-})
